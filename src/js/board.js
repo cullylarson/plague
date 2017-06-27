6b0ec12c699ac4e21e29c999ff3a6bf4af3000b2
Cully Larson <cully.larson@gmai.com>
2017-06-26 22:27:08 -0700

    Made some readability changes, I think.

--- src/js/board.js
+++ src/js/board.js
@@ -7,33 +7,39 @@ const emptyVal = []
 // getByFilter :: (Object -> Boolean) -> Array -> Array([int, int])
 // Get a list of a row, col coordinates in the map that return true when passed to the test function
 const getByFilter = (test, board) =>
-    reduce((acc, row) =>
-        concat(
-            acc,
-            reduce((acc, col) =>
-                concat(acc, test(board[row][col]) ? [[row, col]] : []),
-            [], indexes(board[row]))
-        ),
-    [], indexes(board))
+    reduce(
+        (acc, row) => concat(
+                acc,
+                reduce(
+                    (acc, col) => concat(acc, test(board[row][col]) ? [[row, col]] : []),
+                    [],
+                    indexes(board[row])
+                )
+            ),
+        [],
+        indexes(board)
+    )
 
 export const createEmptyBoard = (width, height) => ofLength(height).map(() => ofLength(width).map(() => emptyVal))
 export const isEmpty = x => x === emptyVal
 export const fillRandom = curry((num, shouldPlace, createUnit, board) => {
-    const result = reduce((acc, _) => {
-        // no spots left
-        if(acc.possibleLocations.length === 0) return acc
+    const result = reduce(
+        (acc, _) => {
+            // no spots left
+            if(acc.possibleLocations.length === 0) return acc
 
-        const locationIndex = randBetween(0, acc.possibleLocations.length - 1)
-        const location = acc.possibleLocations[locationIndex]
+            const locationIndex = randBetween(0, acc.possibleLocations.length - 1)
+            const location = acc.possibleLocations[locationIndex]
 
-        return {
-            board: set(compose(lensIndex(location[0]), lensIndex(location[1])), acc.board[location[0]][location[1]].concat([createUnit()]), acc.board),
-            possibleLocations: omit(locationIndex, acc.possibleLocations),
-        }
-    }, {
-        board: board.slice(),
-        possibleLocations: getByFilter(shouldPlace, board),
-    }, ofLength(num))
+            return {
+                board: set(compose(lensIndex(location[0]), lensIndex(location[1])), acc.board[location[0]][location[1]].concat([createUnit()]), acc.board),
+                possibleLocations: omit(locationIndex, acc.possibleLocations),
+            }
+        }, {
+            board: board.slice(),
+            possibleLocations: getByFilter(shouldPlace, board),
+        }, ofLength(num)
+    )
 
     return result.board
 })
